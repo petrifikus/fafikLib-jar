18e577a628bccb98b2f9fb36ec4322a528b9fafa
fafik77 <55334002+fafik77@users.noreply.github.com>
2024-02-18 17:26:23 +0100

    Delete include/fafikLib/ItemEntryIncl.h
    outdated

--- include/fafikLib/ItemEntryIncl.h
+++
@@ -1,9 +0,0 @@
-#ifndef ITEMENTRYINCL_H
-#define ITEMENTRYINCL_H
-
-#include "fafikLib_DB.h"
-
-///alias fast Call Name of "fafikLib_DB::itemEntry"
-typedef fafikLib_DB::itemEntry ItemEntry;
-
-#endif // ITEMENTRYINCL_H
